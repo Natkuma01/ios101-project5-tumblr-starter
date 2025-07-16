a9e73d4f5a130c6e3ee18c20ed6970af5a1c6b91
Natalie Chan <natkuma@outlook.com>
2025-07-16 02:24:21 -0400

    connected PostCell.swift with table view cell

---
+++ ios101-project5-tumblr/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  ios101-project5-tumblr
+//
+//  Created by Natalie Chan on 7/16/25.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var postTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+      
+    }
+
+  
